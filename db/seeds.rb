5dec35261a2828dd149c9cdabf9f38ccf893a4a9
Felipe <felipeejunges@hotmail.com>
2024-01-04 12:55:13 -0300

    fix seed and docker-compose file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,10 +7,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-2000.times do
+sizes = ["XS", "S", "M", "L", "XL", "XLL"]
+2000.times do |i|
+  @brand = Faker::Company.name if @brand.nil? || i % 10 == 0 
   p = Product.new(name: Faker::Commerce.product_name, description: Faker::Lorem.paragraph, category: Faker::Commerce.department,
-  brand: Faker::Company.name, sizes: [Faker::Lorem.word, Faker::Lorem.word, Faker::Lorem.word], available: true,
+  brand: @brand, sizes: sizes.sample((1..6).to_a.sample), available: true,
   colors: [Faker::Color.color_name, Faker::Color.color_name], tags: [Faker::Lorem.word, Faker::Lorem.word], 
-  original_price: Faker::Commerce.price(range: 50..500.0), discout: Faker::Commerce.price(range: 15..90.0))
-  p.price = original_price - (original_price * discount / 100)
+  original_price: Faker::Commerce.price(range: 50..500.0), discount: Faker::Commerce.price(range: 15..90.0))
+  p.price = p.original_price - (p.original_price * p.discount / 100)
+  p.save
 end
